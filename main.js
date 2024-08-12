a946da721f168f1268494279578cf09e97f3a5c0
z.yurii <zhidetskij@gmail.com>
2024-08-12 18:59:10 +0200

    add "add to calendar" buttons

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 requirejs.config({
     paths: {
-        jquery: 'https://code.jquery.com/jquery-3.6.0.min',
+        jquery: 'https://code.jquery.com/jquery-3.7.1.min',
         bootstrap: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min',
-        app: 'app'
+        app: 'app',
+        ics: 'https://unpkg.com/ics-js@0.10.2/dist/ics-js'
     },
     shim: {
-        'bootstrap': ['jquery']
+        'bootstrap': ['jquery'],
+        'ics': {
+            exports: 'ics'
+        }
     }
 });
-
 requirejs(['jquery', 'bootstrap', 'app'], function($, bootstrap, app) {
     app.init();
 });
