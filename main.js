578824b4f353d16b23e0b11e623b576dbe37f964
z.yurii <zhidetskij@gmail.com>
2024-08-13 11:58:06 +0200

    remove adding bootstrap to requirejs. It fixes error when the navbar-toggler button doesn't close the nav

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 requirejs.config({
     paths: {
-        bootstrap: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min',
         app: 'app',
         ics: 'ics-browserified'
     }
 });
 
-requirejs(['bootstrap', 'app'], function(bootstrap, app) {
+requirejs(['app'], function(app) {
     app.init();
 });
